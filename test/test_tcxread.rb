4173fd99835d6a78bac1fe18bcd2b6abec85baf7
Iztok Fister Jr. <iztok@iztok.dev>
2024-08-17 18:26:16 +0200

    Re-structure tests

--- test/test_tcxread.rb
+++ test/test_tcxread.rb
@@ -7,9 +7,6 @@ class TCXReadTest < Minitest::Test
     # tests for file 2.tcx
     @data1 = TCXRead.new('test/2.tcx')
 
-    # tests for file 15.tcx
-    @data2 = TCXRead.new('test/15.tcx')
-
     # tests for TCX file where watts exist
     @data3 = TCXRead.new('test/23.tcx')
 
@@ -17,23 +14,19 @@ class TCXReadTest < Minitest::Test
 
   def test_total_calories
     assert_equal @data1.total_calories, 924
-    assert_equal @data2.total_calories, 2010
   end
 
   def test_total_distance
       assert_equal @data1.total_distance_meters, 24732.34
-      assert_equal @data2.total_distance_meters, 116366.98
 
   end
 
   def test_total_duration
       assert_equal @data1.total_time_seconds, 3876.0
-      assert_equal @data2.total_time_seconds, 17250.0
   end
 
   def test_total_ascent
       assert_equal @data1.total_ascent, 452.5999946594238
-      assert_equal @data2.total_ascent, 1404.400026500225
   end
 
   def test_NA_watts
